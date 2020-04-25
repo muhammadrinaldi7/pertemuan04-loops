1609d290e48e566bec47853bf2ed343f29389edf
muhammadrinaldi7 <muhammad.rinaldi007.mr@gmail.com>
2020-04-26 02:31:47 +0800

    Perulangan menggunakan FOR pengisian air

---
+++ src/PerulanganFOR.java
@@ -0,0 +1,16 @@
+public class PerulanganFOR {
+    public static void main(String[]args){
+        int isigelas = 0;
+        int penuh = 225;
+
+        System.out.println("Isi Gelas Sekarang : " + isigelas + "m1");
+        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "m1");
+
+        for (isigelas = 0;isigelas <= penuh;isigelas++){
+            System.out.println("Sedang mengisi gelas..");
+            System.out.println("Isi Gelas Sekarang : " + isigelas + "m1");
+        }
+        System.out.println("Finale: Isi Gelas Sekarang : " + isigelas + "m1");
+    }
+
+}
